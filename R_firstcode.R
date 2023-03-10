5936f08c0a767dfa8395b1822c11c108ebc6e980
CarolinaDotoli <127427316+CarolinaDotoli@users.noreply.github.com>
2023-03-10 17:56:17 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -4,3 +4,22 @@
 install.packages("raster")
 
 library(raster)
+
+#Import data, setting the working directory
+ setwd("C:/Lab/") for Windows
+#Import data:
+l2011 <- brick("p224r63_2011_masked.grd")
+
+#plotting the data
+plot(l2011)
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100) #100 sono le sfumature
+
+#plotting one element 
+plot(l2011 [[4]], col=cl)
+#We can use also the name: 
+plot(l2011$B4_sre, col=cl)
+
+
+nir <- l2011[[4]] #or nr <- l2011$B4_sre
+plot(nir, col=cl)
