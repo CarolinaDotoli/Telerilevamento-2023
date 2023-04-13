a0d98ce0741b58b216caa898a8cb16ab239c2d34
CarolinaDotoli <127427316+CarolinaDotoli@users.noreply.github.com>
2023-04-13 17:06:28 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -33,3 +33,41 @@ dift = TGr[[2]] - TGr[[1]]
 
 cl <- colorRampPalette(c("blue", "light blue", "pink", "red")) (100)
 plot(TGr, col=cl)
+
+
+#Exercise 2: European NO2
+setwd("C:/Lab/EN")
+library(raster)
+
+#Importing a file
+EN_1 <- raster("EN_0001.png")
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(EN_1, col=cl)
+
+#Importing all files togheter
+rlist <- list.files(pattern="EN")
+import <- lapply(rlist, raster)
+EN <- stack(import)
+plot(EN)
+
+#Controllare che la prima immagine importanta corrisponda alla prima dello stack
+par(mfrow=c(1,2))
+plot(EN_1, col=cl)
+plot(EN$EN_0001_1, col=cl)
+
+#Second type of check
+difcheck <- EN_1 - EN$EN_0001_1 
+plot(difcheck)
+
+#First and last data
+par(mfrow=c(1,2))
+plot(EN$EN_0001_1, col=cl)
+plot(EN$EN_0013_1, col=cl)
+
+#Differences between the first and the last data
+difen = EN$EN_0001_1 - EN$EN_0013_1
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)
+plot(difen, col=cldif)
+
+plotRGB(EN, r=1, g=7, b=13, stretch="lin") #or plotRGB(EN, 1, 7, 13, stretch="lin")
